06ffb5bb9486c0a5918621f873f2a16a901cd92d
ZeLars3 <zelt378@gmail.com>
2021-05-04 13:37:58 +0300

    Add digit selection buttons

--- index.js
+++ index.js
@@ -4,7 +4,41 @@ const token = '1652765564:AAEHOSGSH5qEKXSAduVlikEW3WAnesIKMAg'
 
 const bot = new TelegramApi(token, {polling: true})
 
+const chats = {}
 
+const gameOptions = {
+    reply_markup: JSON.stringify({
+        inline_keyboard: [
+            [{text: '1', callback_data: '1'}, {text: '2', callback_data: '2'}, {text: '3', callback_data: '3'}],
+            [{text: '4', callback_data: '4'}, {text: '5', callback_data: '5'}, {text: '6', callback_data: '6'}],
+            [{text: '7', callback_data: '7'}, {text: '8', callback_data: '8'}, {text: '9', callback_data: '9'}],
+            [{text: '0', callback_data: '0'}]
+        ]
+    })
+}
+
+const againOptions = {
+    reply_markup: JSON.stringify({
+        inline_keyboard: [
+            [{text: 'Сыграть ещё раз', callback_data: '/again'}]
+        ]
+    })
+}
+
+const startGame = async (chatId) => {
+    await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/9a5/3d6/9a53d66b-53c8-3ccb-a3dd-75fa64c18322/192/42.webp')
+    await bot.sendMessage(chatId, `Смотри, правила игры очень просты - я загадаю число от 0 до 9, а ты попробуешь его отгадать!`)
+
+    const randomNumber = Math.floor(Math.random() * 10)
+    chats[chatId] = randomNumber
+    await bot.sendMessage(chatId, 'Попробуй отгадать', gameOptions)
+}
+
+const againGame = async (chatId) => {
+    const randomNumber = Math.floor(Math.random() * 10)
+    chats[chatId] = randomNumber
+    await bot.sendMessage(chatId, 'Попробуй отгадать', gameOptions)
+}
 
 const start = ()=> {
     bot.setMyCommands([
@@ -26,14 +60,27 @@ const start = ()=> {
             return bot.sendMessage(chatId, `Вот, что я знаю о тебе: \n Имя: ${msg.from.first_name} \n Username: ${msg.from.username}`)
         }
         if (text === '/game'){
-            await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/9a5/3d6/9a53d66b-53c8-3ccb-a3dd-75fa64c18322/192/42.webp')
-            return bot.sendMessage(chatId, `Смотри, правила игры очень просты - я загадаю число от 0 до 9, а ты попробуешь его отгадать!`)
+            return startGame(chatId)
         }
         if(text === '/stop'){
             await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/9a5/3d6/9a53d66b-53c8-3ccb-a3dd-75fa64c18322/8.webp')
             await bot.stop();
         }
         return bot.sendMessage(chatId, 'Подожди, я тебя не понял, давай по-новой)')
+    })
+    bot.on('callback_query', (msg) => {
+        const data = msg.data
+        const chatId = msg.message.chat.id
+
+        if (data === '/again') {
+            return againGame(chatId)
+        }
+        if (data === chats[chatId]) {
+            return bot.sendMessage(chatId, `Поздравляю, ты отгдал цифру ${chats[chatId]}`, againOptions)
+        }
+        else {
+            return bot.sendMessage(chatId, `Увы, но ты не отгдал цифру. Я загадал ${chats[chatId]}`, againOptions)
+        }
     })
 }
 
